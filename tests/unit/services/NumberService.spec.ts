fa876740803415e6f773abb0f8cd38a2dfcd7c4e
Mathieu-S <10650749+Mathieu-S@users.noreply.github.com>
2020-11-24 10:46:46 +0100

    ADD*: NumberService.spec.ts

---
+++ tests/unit/services/NumberService.spec.ts
@@ -0,0 +1,21 @@
+﻿import Axios from "axios";
+import { NumberService } from "@/services";
+
+jest.mock("axios");
+const mockedAxios = Axios as jest.Mocked<typeof Axios>;
+
+describe("NumberService", () => {
+  it("decomposeNumber", async () => {
+    // Arrange
+    const payload = "something";
+    mockedAxios.get.mockResolvedValue({ data: payload });
+    const service = new NumberService();
+
+    // Act
+    const result = await service.decomposeNumber(1);
+
+    // Assert
+    expect(result).toBe(payload);
+    expect(mockedAxios.get).toHaveBeenCalled();
+  });
+});
