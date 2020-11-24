e730240fb9fd1a70151ccc18949baeaa8036bdca
Mathieu-S <10650749+Mathieu-S@users.noreply.github.com>
2020-11-24 10:45:49 +0100

    ADD*: NumberService

---
+++ src/services/NumberService.ts
@@ -0,0 +1,8 @@
+﻿import Axios from "axios";
+
+export class NumberService {
+  public async decomposeNumber(nb: number): Promise<string> {
+    const response = await Axios.get<string>("");
+    return response.data;
+  }
+}
